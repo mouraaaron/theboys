43113cce9806e8d668187986ae38051b4f1eb91a
Aaron Moura <moura.aaron06@gmail.com>
2025-11-04 10:06:11 -0300

    Create theboys.c

---
+++ theboys.c
@@ -0,0 +1,22 @@
+// programa principal do projeto "The Boys - 2024/2"
+// Autor: Aaron Moura, GRR 20255804
+
+// seus #includes vão aqui
+#include <stdio.h>
+
+// seus #defines vão aqui
+
+// minimize o uso de variáveis globais
+
+// programa principal
+int main ()
+{
+  // iniciar o mundo
+
+  // executar o laço de simulação
+
+  // destruir o mundo
+
+  return (0) ;
+}
+
