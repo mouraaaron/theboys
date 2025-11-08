d2577f046250e00547ce124f94acb303650acf82
Aaron Moura <moura.aaron06@gmail.com>
2025-11-08 17:12:07 -0300

    Update entidades.h

--- entidades.h
+++ entidades.h
@@ -1,5 +1,7 @@
 #ifndef entidades_h
 #define entidades_h
+#include "eventos.h"
+#include <stdio.h>
 
 #define T_INICIO 0
 #define T_FIM_DO_MUNDO 525600
@@ -10,13 +12,18 @@
 #define N_MISSOES (T_FIM_DO_MUNDO / 100)
 #define N_COMPOSTOS_V (N_HABILIDADES * 3)
 
-// PERGUNTA 01: Quanto coloco conjuntos, tem q usar ponteiro?????????
-
 // Aqui, tem todas os protótipos das structs das entidades e das funções de criação das entidades.
 struct Heroi;
 struct Base;
 struct Missao;
 struct Mundo;
+
+
+struct Loc
+{
+    int x;
+    int y;
+};
 
 //pergunta -> o que passar nos parametros dessas funções 
 struct Heroi *cria_heroi(int ID);
@@ -39,14 +46,14 @@ struct Base{
     int ID;
     int Lotacao;
     struct cjto_t *Presentes; //tem q usar conjuntos 
-    struct fprio_t *Espera; //fila de espera
-    struct cjto_t *Local; //tem q usar conjunto talvez
+    struct fila_t *Espera; //fila de espera
+    struct Loc Local;
 };
 
 struct Missao{
     int ID;
     struct cjto_t *Habilidades; //tem q usar conjuntos eu acho 
-    struct cjto_t *Local; //tem q usar conjnunto talvez 
+    struct Loc Local;
 };
 
 struct Mundo{
@@ -60,6 +67,7 @@ struct Mundo{
     int NCompostosV;
     int TamanhoMundo; //tem q usar conjunto eu acho 
     long Relogio;   
+    struct fprio_t *LEF;
 
     /* ADENDO: Por que tem que ter dois **? Porque uma struct Heroi **Herois_vivos é um 
     * ponteiro para um vetor de ponteiros, no qual cada espaço é ocupado por uma struct Heroi *
@@ -67,5 +75,28 @@ struct Mundo{
     */
 };
 
+enum TipoEvento
+{
+    EV_Chega,
+    EV_Espera,
+    EV_Desiste,
+    EV_Avisa, 
+    EV_Entra,
+    EV_Sai,
+    EV_Viaja,
+    EV_Morre,
+    EV_Missao,
+    EV_Fim
+};
+
+struct Evento
+{
+    int ID_heroi;
+    int ID_Missao;
+    long Tempo;
+    enum TipoEvento Tipo;
+    int ID_Base;
+
+};
 
 #endif
