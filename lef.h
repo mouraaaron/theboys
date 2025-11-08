9d5817ad6055561f1de00108f9eaf541eeb97158
Aaron Moura <moura.aaron06@gmail.com>
2025-11-08 17:13:20 -0300

    Create lef.h

---
+++ lef.h
@@ -0,0 +1,19 @@
+
+// definir e cirar a LISTA DE EVENTOS FUTUROS L E F
+#ifndef LEF
+#define LEF
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include "fila.h"
+#include "conjunto.h"
+#include "entidades.h"
+#include "eventos.h"
+#include "fprio.h"
+
+
+void incia_lef(struct Mundo *M);
+
+
+#endif
