6141efd5dad9ea46943678512caf5fcd3cd2bed8
Aaron Moura <moura.aaron06@gmail.com>
2025-11-08 17:13:05 -0300

    Update theboys.c

--- theboys.c
+++ theboys.c
@@ -2,7 +2,17 @@
 // Autor: Aaron Moura, GRR 20255804
 
 // seus #includes vão aqui
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include "fila.h"
+#include "conjunto.h"
+#include "entidades.h"
+#include "eventos.h"
+#include "lef.h"
+#include "fprio.h" 
+
 
 // seus #defines vão aqui
 
@@ -11,7 +21,16 @@
 // programa principal
 int main ()
 {
-  // iniciar o mundo
+  srand(time(NULL));
+  struct Mundo *M = cria_mundo();
+  if(!M)
+  {
+    fprintf(stderr, "Erro ao criar o mundo\n");
+    exit(1);
+  }
+
+
+  incia_lef(M);
 
   // executar o laço de simulação
 
