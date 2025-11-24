3080ce77036865b535290b8ad04d547e0d2c5134
Aaron Moura <moura.aaron06@gmail.com>
2025-11-24 14:01:03 -0300

    Update eventos.h

--- eventos.h
+++ eventos.h
@@ -20,7 +20,7 @@ void Desiste(struct Mundo *Mundo, struct Heroi *Heroi, struct Base *Base);
 void Entra(struct Mundo *Mundo, struct Heroi *Heroi, struct Base *Base);
 void Sai(struct Mundo *Mundo, struct Heroi *Heroi, struct Base *Base);
 void Viaja(struct Mundo *Mundo, struct Heroi *Heroi, struct Base *Base);
-void Morre(struct Mundo *Mundo, struct Heroi *Heroi, struct Base *Base);
+void Morre(struct Mundo *Mundo, struct Heroi *Heroi, struct Base *Base, int id_missao);
 void Missao(struct Mundo *Mundo, struct Missao *Missao);
 void Fim(struct Mundo *Mundo);
 void Avisa(struct Mundo *Mundo, struct Base *Base);
