f395227ab5bc58ba93f7d3a205e7b62db9ca5796
Aaron Moura <moura.aaron06@gmail.com>
2025-11-28 03:48:06 -0300

    Refactor function name and clean up includes

--- lef.h
+++ lef.h
@@ -2,18 +2,11 @@
 // definir e cirar a LISTA DE EVENTOS FUTUROS L E F
 #ifndef LEF
 #define LEF
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
-#include <math.h>
-#include "fila.h"
-#include "conjunto.h"
 #include "entidades.h"
-#include "eventos.h"
-#include "fprio.h"
 
 
-void incia_lef(struct Mundo *M);
+void inicia_lef(struct Mundo *Mundo);
 
 
 #endif
