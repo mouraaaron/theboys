3f239f79f438d848342820a552d1554c0573cf66
Aaron Moura <moura.aaron06@gmail.com>
2025-11-29 19:33:10 -0300

    Refactor fila.c for improved item handling

--- fila.c
+++ fila.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "fila.h"
-#include "entidades.h"
 
 
 struct fila_t *fila_cria ()
@@ -10,21 +9,25 @@ struct fila_t *fila_cria ()
     if(!fila)
         return NULL;
     
-    fila->fim = NULL;
+    fila->ult = NULL;
     fila->num = 0;
     fila->prim = NULL;
     return fila;
+
 }
 
+// Libera todas as estruturas de dados da fila, inclusive os itens.
+// Retorno: NULL.
 struct fila_t *fila_destroi (struct fila_t *f)
 {
+    if(f == NULL)
+        return NULL;
+
     struct fila_nodo_t *aux = f->prim;
 
     while(f->num > 0){ //faz a checagem pelo tamanho
         struct fila_nodo_t *prox = aux->prox;
-        free(aux->item); //se o item foi alocado dinamicante, tem q dar free assim
         free(aux);
-        f->num -=1; //diminui o tamanho
         aux = prox;
     }
     free(f);  
@@ -32,76 +35,91 @@ struct fila_t *fila_destroi (struct fila_t *f)
     //igual fprio
 }
 
-int fila_insere (struct fila_t *f, void *item)
+// Insere um item no final da fila (politica FIFO).
+// Retorno: 1 se tiver sucesso ou 0 se falhar.
+int fila_insere (struct fila_t *f, int item)
 {
-    // primeiro, verificar se o item já esta na fila
+    if(f == NULL)
+        return 0;
 
+    
     struct fila_nodo_t *aux = f->prim;
     while(aux != NULL){
-    if(aux->item == item)
-      return -1;
-    aux = aux->prox;
+        if(aux->item == item)
+            return -1; // nao insere se o item ja ta aqui
+        aux = aux->prox;
     }
-
-
-    struct fila_nodo_t *novo_nodo = malloc(sizeof(struct fila_nodo_t)); // cria o nodo q vai ser inserido
+    
+    struct fila_nodo_t *novo_nodo = malloc(sizeof(struct fila_nodo_t)); 
     if(!novo_nodo)
-        return -1;
+        return 0;
     
     novo_nodo->item = item;
-   
-    if(f->num == 0) //caso nao tenha ninguem na fila
+    novo_nodo->prox = NULL; 
+
+  
+    if(f->prim == NULL) 
     {
-        novo_nodo->prox = NULL;
         f->prim = novo_nodo;
-        f->num += 1;
-        f->fim = novo_nodo; 
+        f->ult = novo_nodo; 
     }
+    else 
+    {
      
-    struct fila_nodo_t *nodo_anterior = f->prim;
-    while(nodo_anterior->prox != NULL)
-        nodo_anterior = nodo_anterior->prox; //avança o nodo anterior
-    
-    nodo_anterior->prox = novo_nodo;
+        f->ult->prox = novo_nodo;
+        
+   
+        f->ult = novo_nodo;
+    }
 
-    f->fim = novo_nodo;
-    f->num += 1;
+  
+    f->num++;
 
-
-    return f->num;
+    return 1;
 }
-
-void *fila_retira(struct fila_t *f)
+// Retira o primeiro item da fila e o devolve
+// Retorno 1 se a operação foi bem sucedida e 0 caso contrário
+int fila_retira (struct fila_t *f, int *item)
 {
-    if (!f || f->prim == NULL)
-        return NULL;
+    if (!f || f->prim == NULL || item == NULL)
+        return 0;
 
     // nodo que será removido
     struct fila_nodo_t *nodo = f->prim;
 
-    // pega o item (ponteiro para struct Heroi)
-    void *item = nodo->item;
+    // pega o item (id do heroi)
+    *item = nodo->item;
 
     // atualiza a cabeça da fila
     f->prim = nodo->prox;
-    f->num--;
+    
+    if(f->prim == NULL)
+        f->ult = NULL;
 
+    f->num--;
     // libera apenas o nodo da fila
     free(nodo);
 
-    return item;
+    return 1;
 }
 
+// Informa o número de itens na fila.
+// Retorno: N >= 0 ou -1 se erro.
 int fila_tamanho (struct fila_t *f)
 {
+    if(!f || f->num < 0)
+        return -1;
+
     if(f->num >= 0)
         return f->num;
+    
     return -1;
 }
 
-void fila_imprime(struct fila_t *f)
+// Imprime o conteúdo da fila 
+void fila_imprime (struct fila_t *f)
 {
-    if (!f) {
+        if (!f) {
         printf("Fila NULL.\n");
         return;
     }
@@ -113,11 +131,10 @@ void fila_imprime(struct fila_t *f)
         return;
     }
 
-    while (aux) {
-        printf("%p ", aux->item);
+    while (aux != NULL) {
+        printf("%d ", aux->item);
         aux = aux->prox;
     }
+    
 
-    printf("\n");
 }
-
