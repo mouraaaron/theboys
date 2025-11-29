485b46e1e829ce08377a1b5be97e72fbf2e3f6e2
Aaron Moura <moura.aaron06@gmail.com>
2025-11-29 19:31:41 -0300

    Refactor event handling in inicia_lef function

--- lef.c
+++ lef.c
@@ -1,4 +1,3 @@
-// definir e cirar a LISTA DE EVENTOS FUTUROS L E F
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdlib.h>
@@ -9,9 +8,8 @@
 #include "conjunto.h"
 #include "entidades.h"
 #include <math.h>
-
-
-
+#include "cria_mundo.h"
+#include "lef.h"
 
 void inicia_lef(struct Mundo *Mundo)
 {
@@ -20,73 +18,217 @@ void inicia_lef(struct Mundo *Mundo)
     {
         int prioridade;
         int tipo;
-        struct Evento *ev = fprio_retira(Mundo->LEF,&tipo, &prioridade);
+        void *item = fprio_retira(Mundo->LEF,&tipo, &prioridade);
 
-        if (ev == NULL)
+        if (item == NULL)
             break;
 
-        Mundo->Relogio = ev->Tempo;
-
-        /* Constrói ponteiros reais a partir dos IDs */
-        struct Heroi *H = NULL;
-        struct Base *B = NULL;
-        struct Missao *M = NULL;
-
-        if (ev->ID_heroi >= 0)
-            H = Mundo->Herois_vivos[ev->ID_heroi];
-
-        if (ev->ID_Base >= 0)
-            B = Mundo->Bases[ev->ID_Base];
-
-        if (ev->ID_Missao >= 0)
-            M = Mundo->Missoes[ev->ID_Missao];
-
-        /* Processa o evento correto */
-        switch (ev->Tipo)
+        switch (tipo)
         {
             case EV_Chega:
-                Chega(Mundo, H, B);
+                Chega(Mundo, item);
+                Mundo->ev_tratados++;
                 break;
 
             case EV_Espera:
-                Espera(Mundo, H, B);
+                Espera(Mundo, item);
+                Mundo->ev_tratados++;
                 break;
 
             case EV_Desiste:
-                Desiste(Mundo, H, B);
+                Desiste(Mundo, item);
+                Mundo->ev_tratados++;
                 break;
 
             case EV_Avisa:
-                Avisa(Mundo, B);
+                Avisa(Mundo, item);
+                Mundo->ev_tratados++;
                 break;
 
             case EV_Entra:
-                Entra(Mundo, H, B);
+                Entra(Mundo,item);
+                Mundo->ev_tratados++;
                 break;
 
             case EV_Sai:
-                Sai(Mundo, H, B);
+                Sai(Mundo,item);
+                Mundo->ev_tratados++;
                 break;
 
             case EV_Viaja:
-                Viaja(Mundo, H, B);
+                Viaja(Mundo,item);
+                Mundo->ev_tratados++;
                 break;
 
             case EV_Morre:
-                Morre(Mundo, H, B, ev->ID_Missao);
+                Morre(Mundo,item);
+                Mundo->ev_tratados++;
                 break;
 
             case EV_Missao:
-                Missao(Mundo, M);
+                Missao(Mundo, item);
+                Mundo->ev_tratados++;
                 break;
 
             case EV_Fim:
+                Mundo->ev_tratados++;
                 cond = 0;
-                Fim(Mundo);
+                fim(Mundo);
                 break;
         }
 
-        free(ev);
+        free(item);
     }
 }
 
+void chega_na_lef(struct Mundo *Mundo, struct Evento *evento, int duracao)
+{
+    struct Evento *Chega  = malloc(sizeof(struct Evento));
+    if(Chega == NULL)
+        return;
+    
+    Chega->ID_Base = evento->ID_Base;
+    Chega->ID_heroi = evento->ID_heroi;
+    Chega->ID_Missao = evento->ID_Missao;
+    Chega->Tipo = EV_Chega;
+    Chega->Tempo = evento->Tempo + duracao;
+
+    Mundo->Relogio = evento->Tempo;
+    fprio_insere(Mundo->LEF, Chega, Chega->Tipo, Chega->Tempo);
+}
+
+void espera_na_lef(struct Mundo *Mundo, struct Evento *evento)
+{
+    struct Evento *Espera = malloc(sizeof(struct Evento));
+    if (Espera == NULL)
+        return;
+
+    Espera->ID_Base = evento->ID_Base;
+    Espera->ID_heroi = evento->ID_heroi;
+    Espera->ID_Missao = evento->ID_Missao;
+    Espera->Tipo = EV_Espera;
+    Espera->Tempo = evento->Tempo;  
+
+    Mundo->Relogio = evento->Tempo;
+    fprio_insere(Mundo->LEF, Espera, Espera->Tipo, Espera->Tempo);
+}
+
+void desiste_na_lef(struct Mundo *Mundo, struct Evento *evento)
+{
+    struct Evento *Desiste = malloc(sizeof(struct Evento));
+    if(Desiste == NULL)
+        return;
+
+    Desiste->ID_Base = evento->ID_Base;
+    Desiste->ID_heroi = evento->ID_heroi;   
+    Desiste->ID_Missao = evento->ID_Missao;
+    Desiste->Tipo = EV_Desiste;
+    Desiste->Tempo = evento->Tempo;   
+
+    Mundo->Relogio = evento->Tempo;
+    fprio_insere(Mundo->LEF, Desiste, Desiste->Tipo, Desiste->Tempo);
+}
+
+void avisa_na_lef(struct Mundo *Mundo, struct Evento *evento)
+{
+    struct Evento *Avisa = malloc(sizeof(struct Evento));
+    if (Avisa == NULL)
+        return;
+
+    Avisa->ID_Base = evento->ID_Base;
+    Avisa->ID_heroi = evento->ID_heroi;
+    Avisa->ID_Missao = evento->ID_Missao;
+    Avisa->Tipo = EV_Avisa;
+    Avisa->Tempo = evento->Tempo;   
+
+    Mundo->Relogio = evento->Tempo;
+    fprio_insere(Mundo->LEF, Avisa, Avisa->Tipo, Avisa->Tempo);
+
+}
+
+void viaja_na_lef(struct Mundo *Mundo, struct Evento *evento, int base_nova)
+{
+    struct Evento *Viaja = malloc(sizeof(struct Evento));
+    if(Viaja == NULL)
+        return;
+    
+    Viaja->Tempo = evento->Tempo;
+    Viaja->Tipo = EV_Viaja;
+    Viaja->ID_heroi = evento->ID_heroi;
+    Viaja->ID_Base = evento->ID_Base;
+    Viaja->ID_Missao = evento->ID_Missao;
+    Viaja->destino = base_nova;
+
+    fprio_insere(Mundo->LEF, Viaja, Viaja->Tipo, Viaja->Tempo);
+
+}
+
+void entra_na_lef(struct Mundo *Mundo, struct Evento *evento)
+{
+    struct Evento *Entra = malloc(sizeof(struct Evento));
+    if (Entra == NULL)
+        return;
+
+    Entra->ID_Base = evento->ID_Base;
+    Entra->ID_heroi = evento->ID_heroi;
+    Entra->ID_Missao = evento->ID_Missao;
+    Entra->Tipo = EV_Entra;
+    Entra->Tempo = evento->Tempo;  
+
+    Mundo->Relogio = evento->Tempo;
+    fprio_insere(Mundo->LEF, Entra, Entra->Tipo, Entra->Tempo);
+
+}
+
+void sai_na_lef(struct Mundo *Mundo, struct Evento *evento, int TPB)
+{
+    struct Evento *Sai = malloc(sizeof(struct Evento));
+    if(Sai == NULL)
+        return;
+    
+    Sai->Tipo = EV_Sai;
+    Sai->Tempo = evento->Tempo + TPB;
+    Sai->ID_heroi = evento->ID_heroi;
+    Sai->ID_Base = evento->ID_Base;
+    Sai->ID_Missao = evento->ID_Missao;
+    
+
+    Mundo->Relogio = evento->Tempo;
+
+    fprio_insere(Mundo->LEF, Sai, Sai->Tipo, Sai->Tempo);
+}
+
+void morre_na_lef(struct Mundo *Mundo, struct Evento *evento, int id_heroi, int base_que_morre)
+{
+    struct Evento *Morre = malloc(sizeof(struct Evento));
+    if (Morre == NULL) {
+        return;
+    }
+
+    Morre->ID_Missao = evento->ID_Missao;
+    Morre->ID_Base = base_que_morre;
+    Morre->ID_heroi = id_heroi;
+    Morre->Tipo = EV_Morre;
+    Morre->Tempo = evento->Tempo;
+
+    Mundo->Relogio = evento->Tempo;
+
+    fprio_insere(Mundo->LEF, Morre, Morre->Tipo, Morre->Tempo);
+}
+
+void missao_na_lef(struct Mundo *Mundo, struct Evento *evento)
+{
+    struct Evento *Missao = malloc(sizeof(struct Evento));
+    if (Missao == NULL)
+        return;
+
+    Missao->ID_Base = evento->ID_Base;
+    Missao->ID_heroi = evento->ID_heroi;
+    Missao->ID_Missao = evento->ID_Missao;
+    Missao->Tipo = EV_Missao;
+    Missao->Tempo = evento->Tempo;   
+
+    Mundo->Relogio = evento->Tempo;
+    fprio_insere(Mundo->LEF, Missao, Missao->Tipo, Missao->Tempo);
+
+}
